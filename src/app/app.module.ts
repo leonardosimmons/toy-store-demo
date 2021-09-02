894dd28d196de25604922b03be5b8482f82640e0
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-09-02 04:03:35 -0700

    updated for start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule
